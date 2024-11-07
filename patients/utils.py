3df857efaae95dc9f7047e04fbcd989add53c69b
Kurdupov <60926953+Kurdupov@users.noreply.github.com>
2024-11-07 21:50:35 +0200

    Delete patients directory

--- patients/utils.py
+++
@@ -1,7 +0,0 @@
-# patients/utils.py
-
-import hashlib
-
-def create_blockchain_record(patient_id, diagnosis, visit_date, analysis_results, previous_hash=""):
-    record = f'{patient_id}{diagnosis}{visit_date}{analysis_results}{previous_hash}'
-    return hashlib.sha256(record.encode('utf-8')).hexdigest()
